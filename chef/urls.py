24c8e999aa5a95cb1fc1e77f283542a4911f780f
rekto167 <ekysr99@gmail.com>
2021-01-29 21:07:11 +0700

    tambah login

--- chef/urls.py
+++ chef/urls.py
@@ -5,10 +5,13 @@ from .views import (
     IndexChefView,
     MenuList,
     AddMenu,
+    MenuCategoryList,
 )
 
 app_name = 'chef'
 urlpatterns = [
+    path('listmenu/category/<kategori>',
+         MenuCategoryList.as_view(), name='category'),
     path('addmenu/', AddMenu.as_view(), name='addmenu'),
     path('listmenu/<page>', MenuList.as_view(), name='listmenu'),
     path('', IndexChefView.as_view(), name='index'),
--- chef/views.py
+++ chef/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View, TemplateView, ListView, CreateView
 from django.core.files.storage import FileSystemStorage
 from django.contrib import messages
 from django.urls import reverse_lazy, reverse
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 from .models import MenuModel
@@ -15,8 +16,29 @@ class IndexChefView(View):
         'judul': 'Halaman Login Chef',
     }
 
+    def post(self, request, *args, **kwargs):
+        username_login = request.POST['username']
+        password_login = request.POST['password']
+        user = authenticate(request, username=username_login,
+                            password=password_login)
+        if user is not None:
+            login(request, user)
+        return redirect('chef:listmenu')
+
     def get(self, request, *args, **kwargs):
         return render(self.request, self.template_name, self.context)
+
+
+class MenuCategoryList(ListView):
+    model = MenuModel
+    template_name = 'chef/chef_category_menu.html'
+    context_object_name = 'menu_category'
+    paginate_by = 4
+
+    def get_queryset(self):
+        menu_category = self.model.objects.filter(
+            food_cat=self.kwargs['kategori'])
+        return super().get_queryset()
 
 
 class MenuList(ListView):
@@ -28,8 +50,6 @@ class MenuList(ListView):
             'food_cat', flat=True).distinct()
         self.kwargs.update({'category_list': category_menu})
         kwargs = self.kwargs
-        # context = super().get_context_data(**kwargs)
-        # context['page_title'] = 'Daftar Menu'
         return super().get_context_data(*args, **kwargs)
 
 
